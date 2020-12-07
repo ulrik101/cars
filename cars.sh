60c8ded854df3025e1cebad4fef27e617268b724
ulrik101 <ulrik101@mail.chapman.edu>
2020-12-06 20:23:00 -0800

    working base

--- cars.sh
+++ cars.sh
@@ -1,3 +1,26 @@
 #! /bin/bash
 # cars.sh
 # David Ulriksen
+
+while true
+do
+    echo "Type ""1"" to Add a car: "
+    echo "Type ""2"" to List the cars in the inventory file: "
+    echo "Type ""3"" to Quit the program: "
+
+    read userInput
+    case $userInput in
+    1)
+        break
+        ;;
+    2)
+        break
+        ;;
+    3)
+        break
+        ;;
+    *)
+        echo "Input not in range: "
+        ;;
+    esac
+done
